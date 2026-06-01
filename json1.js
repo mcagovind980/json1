6ba07deeed65deebf851254dab5846bca0ecf5e3
mcagovind980 <kg878672@gmail.com>
2026-06-02 01:13:20 +0530

    t

--- json1.js
+++ json1.js
@@ -429,4 +429,26 @@
 
 
 
+function createCounter() {
+ let count = 0; 
+
+  return function () {
+
+    count++;
+    return count;
+    };
+ }
+
+const counterA = createCounter();
+const counterB = createCounter();
+
+console.log(counterA()); // 1
+console.log(counterA()); // 2
+console.log(counterA()); // 3
+
+console.log(counterB()); // 1
+console.log(counterB()); // 2
+
+console.log(counterA()); // 4
+
 
